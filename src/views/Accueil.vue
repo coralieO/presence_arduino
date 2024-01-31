a3929da7694a20d1c1c624c0cb76ef49eec61a5c
coralieO <coralie.ouedraogo@edu.devinci.fr>
2024-01-31 09:38:28 +0100

    add alerte

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,7 +1,9 @@
 <template>
     <div class=" bg-slate-100">
         <div class="h-12 bg-cyan-700"></div>
+        <div class="px-12 pt-3">
+            <h1 class="text-4xl my-3 text-cyan-600">Bienvenue sur le portail de l'IIM</h1>
 
-        <h1 class=" text-amber-600">Bienvenue sur le portail IIM</h1>
+        </div>
     </div>
 </template>
--- src/views/Presences.vue
+++ src/views/Presences.vue
@@ -3,21 +3,22 @@
     <div class=" ">
         <div class="h-12 bg-cyan-700"></div>
         <div class="bg-slate-100 px-12 pt-3">
-            <h1 class="text-4xl my-3 text-black">Listes des presences</h1>
-
-        <div class="grid grid-cols-2 gap-6 mt-12">
+            <h1 class="text-4xl my-3 text-cyan-600">Listes des presences</h1>
+            <div v-if="New" class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-100" role="alert">
+                <span class="font-medium">{{ New }} est présent</span>
+            </div>
+        <div class="grid grid-cols-2 gap-8 mt-12">
             <div>
-                <h2 class="text-xl text-black mb-4">Présents</h2>
-                <div class=" h-96 overflow-auto grid grid-cols-2 gap-2">
-
-                <div class="rounded-2xl" v-for="student in PresentUsers" :key="student.id">
-                    <Table :Name="student.name"  :Mail="student.email"/>
-                </div>
+                <h2 class="text-2xl text-center text-cyan-600 mb-4">Présents</h2>
+                <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
+                    <div v-for="student in PresentUsers" :key="student.id">
+                        <Table :Name="student.name"  :Mail="student.email"/>
+                    </div>
                 </div>
             </div>
             <div>
-                <h2 class="text-2xl text-center text-black mb-4">Absents</h2>
-                <div class=" h-96 overflow-auto grid grid-cols-2 gap-2">
+                <h2 class="text-2xl text-center text-cyan-600 mb-4">Absents</h2>
+                <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
                     <div  v-for="student in Abstudent" :key="student.id">
                     <Table :Name="student.name"  :Mail="student.email" :absent="true" />
                     </div>
@@ -42,11 +43,14 @@ export default defineComponent({
   setup() {
     const Abstudent= ref([]);
     const PresentUsers = ref([]);
+    const New = ref();
     const evtSource = new EventSource("https://iot-m2-card.onrender.com/sse/1", {
     withCredentials: false,
     });
     evtSource.onmessage = (event) => {
         fetchData();
+        New.value = event.presentUsers.name
+
     };
     // Fonction pour récupérer des données
     const fetchData = async () => {
@@ -67,7 +71,8 @@ export default defineComponent({
 
     return { 
         Abstudent,
-        PresentUsers
+        PresentUsers,
+        New
      };
   },
 })
