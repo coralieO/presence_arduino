a6475faf8143cbfed871ca19e364832abad08f32
Lucas <lucas.daval@edu.devinci.fr>
2024-01-31 11:55:01 +0100

    FIX - Sound notification when a student is present

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,38 +1,26 @@
 <template>
-    <div class="content bg-white border-b-2 border-green-500 px-3 py-2 mb-3
-     text-black text-center" :class="{ '!border-red-500': absent}">
-        <div class="name" v-on:change="sayHello">
-            <p class="font-bold">{{Name}}</p>
-            <p class="pt-1  text-sm">{{ Mail }}</p>
-            <button @click="sayHello">speak</button>
-        </div>
-     
+  <div
+    class="content bg-white border-b-2 border-green-500 px-3 py-2 mb-3 text-black text-center"
+    :class="{ '!border-red-500': absent }"
+  >
+    <div class="name">
+      <p class="font-bold">{{ Name }}</p>
+      <p class="pt-1 text-sm">{{ Mail }}</p>
     </div>
+  </div>
 </template>
 
-
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: {
-        Name: String,
-        Mail: String,
-        studentState: Boolean,
-        absent: Boolean
-    },
-  setup(props) {
-    const sayHello = () => {
-      var msg = new SpeechSynthesisUtterance();
-      msg.text = "Bienvenu " + props.Name;
-      msg.lang = "fr";
-      window.speechSynthesis.speak(msg);
-    };
-    return { sayHello };
-    },
-})
+  props: {
+    Name: String,
+    Mail: String,
+    studentState: Boolean,
+    absent: Boolean,
+  },
+  setup() {},
+});
 </script>
-<style>
-
-
-</style>
+<style></style>
--- src/views/Presences.vue
+++ src/views/Presences.vue
@@ -1,69 +1,85 @@
 <template>
-        
-    <div class=" ">
-        <div class="h-12 bg-cyan-700"></div>
-        <div class="bg-slate-100 px-12 pt-3">
-            <h1 class="text-4xl my-3 text-cyan-600">Listes des presences</h1>
-            <div v-if="New" class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-100" role="alert">
-                <span class="font-medium">{{ New }} est présent</span>
+  <div class=" ">
+    <div class="h-12 bg-cyan-700"></div>
+    <div class="bg-slate-100 px-12 pt-3">
+      <h1 class="text-4xl my-3 text-cyan-600">Listes des presences</h1>
+      <div
+        v-if="New"
+        class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-100"
+        role="alert"
+      >
+        <span class="font-medium">{{ New }} est présent</span>
+      </div>
+      <div class="grid grid-cols-2 gap-8 mt-12">
+        <div>
+          <h2 class="text-2xl text-center text-cyan-600 mb-4">Présents</h2>
+          <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
+            <div v-for="student in PresentUsers" :key="student.id">
+              <Table :Name="student.name" :Mail="student.email" />
             </div>
-        <div class="grid grid-cols-2 gap-8 mt-12">
-            <div>
-                <h2 class="text-2xl text-center text-cyan-600 mb-4">Présents</h2>
-                <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
-                    <div v-for="student in PresentUsers" :key="student.id">
-                        <Table :Name="student.name"  :Mail="student.email"/>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <h2 class="text-2xl text-center text-cyan-600 mb-4">Absents</h2>
-                <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
-                    <div  v-for="student in Abstudent" :key="student.id">
-                    <Table :Name="student.name"  :Mail="student.email" :absent="true" />
-                    </div>
-                </div>
-                
-            </div>
+          </div>
         </div>
+        <div>
+          <h2 class="text-2xl text-center text-cyan-600 mb-4">Absents</h2>
+          <div class="h-96 overflow-auto grid grid-cols-2 gap-2">
+            <div v-for="student in Abstudent" :key="student.id">
+              <Table
+                :Name="student.name"
+                :Mail="student.email"
+                :absent="true"
+              />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent, onMounted,ref } from 'vue'
-import axios from 'axios'
-import Table from '../components/Table.vue';
+import { defineComponent, onMounted, ref } from "vue";
+import axios from "axios";
+import Table from "../components/Table.vue";
 
 export default defineComponent({
-    components:{
-        Table,
-    },
+  components: {
+    Table,
+  },
 
   setup() {
-    const Abstudent= ref([]);
+    const Abstudent = ref([]);
     const PresentUsers = ref([]);
     const New = ref();
-    const evtSource = new EventSource("https://iot-m2-card.onrender.com/sse/1", {
-    withCredentials: false,
-    });
+    const evtSource = new EventSource(
+      "https://iot-m2-card.onrender.com/sse/1",
+      {
+        withCredentials: false,
+      }
+    );
     evtSource.onmessage = (event) => {
-        fetchData();
-        const ParseJson= JSON.parse(event.data)
-        New.value = ParseJson.data.user.name
-        console.log( New.value);
+      fetchData();
+      const ParseJson = JSON.parse(event.data);
+      New.value = ParseJson.data.user.name;
 
+      var msg = new SpeechSynthesisUtterance();
+
+      msg.text = "Bienvenue " + ParseJson.data.user.name;
+
+      msg.lang = "fr";
+
+      speechSynthesis.speak(msg);
     };
     // Fonction pour récupérer des données
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://iot-m2-card.onrender.com/rooms/1');
-        console.log(response.data);
-        Abstudent.value = response.data.absentUsers 
-        PresentUsers.value = response.data.presentUsers 
+        const response = await axios.get(
+          "https://iot-m2-card.onrender.com/rooms/1"
+        );
+        Abstudent.value = response.data.absentUsers;
+        PresentUsers.value = response.data.presentUsers;
         // Traitez les données reçues ici
       } catch (error) {
-        console.error('Erreur lors de la récupération des données:', error);
+        console.error("Erreur lors de la récupération des données:", error);
       }
     };
 
@@ -71,15 +87,11 @@ export default defineComponent({
       fetchData();
     });
 
-    return { 
-        Abstudent,
-        PresentUsers,
-        New
-     };
+    return {
+      Abstudent,
+      PresentUsers,
+      New,
+    };
   },
-})
-
-
-
-
+});
 </script>
